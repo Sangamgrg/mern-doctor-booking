d1f1c0b4a47f6f83816522ba908648e406415f41
sangam <sangamgrg89@gmail.com>
2023-12-19 18:48:37 +1100

    frontend and backend project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+function App() {
+  return (
+    <>
+      <div className=" border-2 h-60 w-1/2 shadow-md shadow-panelShadow">
+        <h1 className="text-lg text-yellowColor">My first app</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
