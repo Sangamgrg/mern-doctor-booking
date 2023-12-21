8f0080247111ba509dc58c2d5e943dcaf711b6bd
sangam <sangamgrg89@gmail.com>
2023-12-21 23:43:45 +1100

    route and routing path setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,8 @@
 import './App.css';
+import Layout from './layout/layout';
+
 function App() {
-  return (
-    <>
-      <div className=" border-2 h-60 w-1/2 shadow-md shadow-panelShadow">
-        <h1 className="text-lg text-yellowColor">My first app</h1>
-      </div>
-    </>
-  );
+  return <Layout />;
 }
 
 export default App;
