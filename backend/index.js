570cee81437aa8cd0846f8712437819eb81248a2
sangam <sangamgrg89@gmail.com>
2024-01-18 22:06:51 +1100

    Backend middleware and database connection setup

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,39 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const port = process.env.PORT || 8000;
+
+const corsOptions = {
+  origin: true,
+};
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('App is working');
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT} `);
+//database connection
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('MongoDB connection successful!!!');
+  } catch (err) {
+    console.log('MongoDB connection failed!!! ');
+  }
+};
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+app.listen(port, () => {
+  connectDB();
+  console.log('Server is running on port' + port);
 });
