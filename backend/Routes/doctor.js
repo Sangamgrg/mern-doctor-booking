28477bff12fff59aca46832503162b2b5bddf11f
sangam <sangamgrg89@gmail.com>
2024-01-28 12:47:38 +1100

    create a doctor review and implement review  route'

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -8,7 +8,12 @@ import {
 
 import { authenticate, restrict } from '../auth/verifyToken.js';
 
+import reviewRouter from './review.js';
+
 const router = express.Router();
+
+//nested route for reviews for each doctor
+router.use('/:id/reviews', reviewRouter);
 
 router.get('/:id', getSingleDoctor);
 router.get('/', getAllDoctor);
